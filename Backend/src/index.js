07c854f00852e5581c76e375c0ceba243e96278c
hamidmeer4 <hmdm3r@gmail.com>
2025-10-02 21:40:49 +0500

    i create atleast 5 practice api

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,30 +1,49 @@
 // Import express
 const express = require("express");
-
 // Create an instance of express
 const app = express();
+app.use(express.json())
 
-/*
-1) The `app.use()` method is middleware. 
-   It runs for every request (no matter which route).
-   But if you want to create specific routes like "/", "/about", use `app.get()` instead.
-
-2) Always send a response (`res.send`, `res.json`, etc.) otherwise the request will hang.
-
-3) Use a port variable (best practice for deployment).
-*/
-
-// Default route (Home Page)
-// app.use("/", (req, res) => {
-//   res.send("Hello from Home Page!");
-// });
-
-app.use("/about", (req, res) => {
-  res.send("This is the About Page.");
+// 3 api's
+app.get("/about", (req, res) => {
+  res.send("Hi my nmae is hamid meer and im from pakistan...");
+});
+app.get("/contact-us", (req, res) => {
+  res.send("Here for contact number...");
+});
+app.get("/faqs", (req, res) => {
+  res.send("now the faq is about the project project...");
 });
 
+app.get("/", (req, res) => {
+  res.send("Hello from Home Page!");
+});
+
+// post 
+
+const users  = [];
+app.post("/addUser",(req,res)=> {
+
+    const body = req.body;
+    const newUser = {
+        firstname : "Hamid",
+        lastname : "Bangash",
+    }
+    users.push(newUser)
+    res.json({
+        message : "Added User Successfully",
+        data: users
+    })
+})
+
+app.get("/getAllUser", (req,res) => {
+ res.json({
+    users
+ })
+})
+
 // Start the server on port 3000
-app.listen(3001, () => {
+app.listen(7000, () => {
   console.log("✅ Server is running on http://localhost:3000");
 });
 
