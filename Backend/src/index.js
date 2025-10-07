adb846fc0ff9dbf315ecee2da979263151ef5809
hamidmeer4 <hmdm3r@gmail.com>
2025-10-07 20:20:01 +0500

    i completed the crud routes

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -2,62 +2,73 @@
 const express = require("express");
 // Create an instance of express
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
-// 3 api's
-app.get("/about", (req, res) => {
-  res.send("Hi my nmae is hamid meer and im from pakistan...");
+const users = [];
+var indexId = 1;
+app.post("/addUser", (req, res) => {
+  const body = req.body;
+  users.push({
+    id: indexId++,
+    name: body.name,
+    email: body.email,
+    age: body.age,
+  });
+
+  res.json({
+    message: "Added User Successfully",
+    data: users,
+  });
 });
-app.get("/contact-us", (req, res) => {
-  res.send("Here for contact number...");
-});
-app.get("/faqs", (req, res) => {
-  res.send("now the faq is about the project project...");
-});
 
-app.get("/", (req, res) => {
-  res.send("Hello from Home Page!");
+app.get("/getAllUser", (req, res) => {
+  res.json({
+    users,
+  });
 });
 
-// post 
+// crating a delete api
 
-const users  = [];
-app.post("/addUser",(req,res)=> {
+app.delete("/userDeleted/:id", (req, res) => {
+  const userId = parseInt(req.params.id);
+  const index = users.findIndex((user) => user.id === userId);
 
-    const body = req.body;
-    const newUser = {
-        firstname : "Hamid",
-        lastname : "Bangash",
-    }
-    users.push(newUser)
-    res.json({
-        message : "Added User Successfully",
-        data: users
-    })
-})
+  if (index !== -1) {
+    users.splice(index, 1); //remove user from array
+    return res.json({
+      message: "User Successfully deleted...",
+      data: users,
+    });
+  } else {
+    return res.status(404).json({ message: "something error inside the api" });
+  }
+});
 
-app.get("/getAllUser", (req,res) => {
- res.json({
-    users
- })
-})
+app.put("/updateUser/:id", (req, res) => {
+  const userId = parseInt(req.params.id);
+  const body = req.body;
+  const index = users.findIndex((user) => user.id === userId);
+
+  if (index !== -1) {
+    users[index] = {
+      ...users[index], // keep old data (like id)
+      name: body.name || users[index].name,
+      email: body.email || users[index].email,
+      age: body.age || users[index].age,
+    };
+
+    return res.json({
+      message: "User successfully updated...",
+      data: users[index],
+    });
+  } else {
+    return res.status(404).json({
+      message: "User not found",
+    });
+  }
+});
 
 // Start the server on port 3000
 app.listen(7000, () => {
-  console.log("✅ Server is running on http://localhost:3000");
+  console.log("✅ Server is running on http://localhost:7000");
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
